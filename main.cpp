e5d8e31258740fd8334fc697611500fc5f903e88
Miguel Espinosa <miguelespinosaiv@gmail.com>
2021-02-15 21:58:09 -0500

    Basic while loop completed

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
-//
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+  //original message to display
+  std::cout << "Enter QUIT to exit" << endl;
+
+  //infinite loop
+  while(1){
+
+    string response;
+    string quit = "QUIT";
+    std::cout << "m-shell-0.1$ ";
+    getline(cin, response);
+
+    //exits once user input equals 'QUIT'
+    if(quit.compare(response) == 0){
+      std::cout << "Exiting..." << endl;
+      return 0;
+    }
+
+  }
+
+}
